862201462ab6311d09b308c5bcdf528482f5e8bc
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-04-30 22:37:14 +0100

    Add library array, object constructor and a function to store book objects

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hello")
+const myLibrary = [];
+
+function Book(title, author, publisher, dateOfPublication, pages, read) {
+    if (!new.target) {
+        throw 'Error: the "new" keyword must preceed function';
+    }
+
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.publisher = publisher;
+    this.dateOfPublication = dateOfPublication;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, publisher, dateOfPublication, pages, read) {
+    let newBook = new Book(title, author, publisher, dateOfPublication, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary(
+    'A Man and His Mission'
+    , 'Adrian Cluade Gild'
+    , 'Gospel Advocate'
+    , '2006'
+    , 300
+    , true
+);
+
+console.table(myLibrary);
