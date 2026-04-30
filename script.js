b3c1f9ab9ec0bb894bbc3e2645bc9eaa4d291cd0
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-04-30 23:17:12 +0100

    Edit displayLibraryBooks function so that book details are displayed on the viewport rather than the console

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector(".wrapper");
+
 const myLibrary = [];
 
 function Book(title, author, publisher, dateOfPublication, pages, read) {
@@ -43,13 +45,53 @@ addBookToLibrary(
     , 'John, the apostle'
     , 'A. D. 90'
     , NaN
-    , true
+    , false
 );
 
 console.table(myLibrary);
 
 function displayLibraryBooks(library) {
-    myLibrary.forEach((book) => console.log(book));
+    myLibrary.forEach(book => {
+        let bookCard = document.createElement("div");
+        bookCard.setAttribute("class", "book");
+
+        let bookId = document.createElement("p");
+        bookId.setAttribute("id", "book-id");
+        bookId.textContent = book.id;
+
+        let bookTitle = document.createElement("p");
+        bookTitle.setAttribute("id", "book-title");
+        bookTitle.textContent = book.title;
+
+        let bookAuthor = document.createElement("p");
+        bookAuthor.setAttribute("id", "book-author");
+        bookAuthor.textContent = book.author;
+
+        let bookPublisher = document.createElement("p");
+        bookPublisher.setAttribute("id", "book-publisher");
+        bookPublisher.textContent = book.publisher;
+
+        let dateOfPublication = document.createElement("p");
+        dateOfPublication.setAttribute("id", "date-of-publication");
+        dateOfPublication.textContent = book.dateOfPublication;
+
+        let bookPages = document.createElement("p");
+        bookPages.setAttribute("id", "book-pages");
+        bookPages.textContent = book.pages;
+
+        let readStatus = document.createElement("p");
+        readStatus.setAttribute("id", "read-status");
+        readStatus.textContent = (book.read == true) ? "Read" : "Yet to read";
+
+        bookCard.appendChild(bookId);
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPublisher);
+        bookCard.appendChild(dateOfPublication);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(readStatus);
+        display.appendChild(bookCard);
+    });
 }
 
 displayLibraryBooks(myLibrary);
